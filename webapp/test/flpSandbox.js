cc65bfbb51dfc8e2bd695574027f6731c4e5fae4
Rizwan Trainings <rizwantrainingsb70p2@gmail.com>
2024-01-10 03:13:31 +0000

    completeflow

---
+++ webapp/test/flpSandbox.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+	"sap/base/util/ObjectPath",
+	"sap/ushell/services/Container"
+], function (ObjectPath) {
+	"use strict";
+
+	// define ushell config
+	ObjectPath.set(["sap-ushell-config"], {
+		defaultRenderer: "fiori2",
+		bootstrapPlugins: {
+			"RuntimeAuthoringPlugin": {
+				component: "sap.ushell.plugins.rta",
+				config: {
+					validateAppVersion: false
+				}
+			},
+			"PersonalizePlugin": {
+				component: "sap.ushell.plugins.rta-personalize",
+				config: {
+					validateAppVersion: false
+				}
+			}
+		},
+		renderers: {
+			fiori2: {
+				componentData: {
+					config: {
+						enableSearch: false,
+						rootIntent: "Shell-home"
+					}
+				}
+			}
+		},
+		services: {
+			"LaunchPage": {
+				"adapter": {
+					"config": {
+						"groups": [{
+							"tiles": [{
+								"tileType": "sap.ushell.ui.tile.StaticTile",
+								"properties": {
+									"title": "B70 SAPUI5 Application",
+									"targetURL": "#comdemob70ui5app-display"
+								}
+							}]
+						}]
+					}
+				}
+			},
+			"ClientSideTargetResolution": {
+				"adapter": {
+					"config": {
+						"inbounds": {
+							"comdemob70ui5app-display": {
+								"semanticObject": "comdemob70ui5app",
+								"action": "display",
+								"description": "A Fiori application.",
+								"title": "B70 SAPUI5 Application",
+								"signature": {
+									"parameters": {}
+								},
+								"resolutionResult": {
+									"applicationType": "SAPUI5",
+									"additionalInformation": "SAPUI5.Component=com.demo.b70ui5app",
+									"url": sap.ui.require.toUrl("com/demo/b70ui5app")
+								}
+							}
+						}
+					}
+				}
+			},
+			NavTargetResolution: {
+				config: {
+					"enableClientSideTargetResolution": true
+				}
+			}
+		}
+	});
+
+	var oFlpSandbox = {
+		init: function () {
+			/**
+			 * Initializes the FLP sandbox
+			 * @returns {Promise} a promise that is resolved when the sandbox bootstrap has finshed
+			 */
+
+			// sandbox is a singleton, so we can start it only once
+			if (!this._oBootstrapFinished) {
+				this._oBootstrapFinished = sap.ushell.bootstrap("local");
+				this._oBootstrapFinished.then(function () {
+					sap.ushell.Container.createRenderer().placeAt("content");
+				});
+			}
+
+			return this._oBootstrapFinished;
+		}
+	};
+
+	return oFlpSandbox;
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/demo/b70ui5app/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
